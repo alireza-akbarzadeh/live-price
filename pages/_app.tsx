c4bcd22fc320981e6c071d496271212f1892b522
AlirezaDev951 <devtools95@gmail.com>
2022-06-26 17:43:11 +0430

    initial commit with superplate

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { AppProps } from "next/app";
+import "@styles/global.css";
+import { appWithTranslation } from "@i18n";
+
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
