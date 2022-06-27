a51c6ec5360c55e381b001bdd194d68487238e14
AlirezaDev951 <devtools95@gmail.com>
2022-06-27 20:07:42 +0430

    issue: Type error: Cannot find module 'next/Image' or its corresponding type declarations.

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -3,6 +3,11 @@ import Image from "next/image";
 
 export const Logo: React.FC = () => {
   return (
-    <Image src="/images/header-logo.18e678a4.svg" alt="bitbarg" width="120" height="58" />
+    <Image
+      src="/images/header-logo.18e678a4.svg"
+      alt="bitbarg"
+      width="120"
+      height="58"
+    />
   );
 };
